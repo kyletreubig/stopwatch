dda143527161a4ba7143ad9d51eb4affe184ca51
Kyle Treubig <kyle@vimofthevine.com>
2025-04-13 23:02:21 +0000

    Remove counter example from top-level app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
-
 import { DateSelection } from "./components/date-selection";
-import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container mx-auto flex h-screen flex-col gap-4">
       <h1 className="mt-2">Stopwatch</h1>
       <DateSelection />
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </div>
     </div>
   );
 }
