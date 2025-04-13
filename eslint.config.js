82e075b95c8cce5f8e623ea4dceeb9d5a1d213fc
Kyle Treubig <kyle@vimofthevine.com>
2025-04-13 19:28:45 +0000

    Configure import eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -4,14 +4,35 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   ...pluginQuery.configs["flat/recommended"],
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ["eslint.config.js"],
+    rules: {
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["typescript-eslint"],
+        },
+      ],
+    },
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+        },
+      },
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -25,6 +46,22 @@ export default tseslint.config(
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
       ],
       "@typescript-eslint/no-explicit-any": ["off"],
     },
