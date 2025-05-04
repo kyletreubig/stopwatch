3de781c8b4235b3748568b4ec4895151c87cda25
Kyle Treubig <kyle@vimofthevine.com>
2025-05-04 19:56:04 +0000

    Show updated end time for active timer entries

--- src/components/project-select.tsx
+++ src/components/project-select.tsx
@@ -1,6 +1,7 @@
-import { type SelectProps } from "@radix-ui/react-select";
+import type { SelectTriggerProps, SelectProps } from "@radix-ui/react-select";
 
 import { useProjects } from "@/api/get-projects";
+import { cn } from "@/lib/utils";
 
 import {
   Select,
@@ -10,11 +11,15 @@ import {
   SelectValue,
 } from "./ui/select";
 
-export function ProjectSelect(props: SelectProps) {
+type Props = SelectProps & {
+  className: SelectTriggerProps["className"];
+};
+
+export function ProjectSelect({ className, ...props }: Props) {
   const projects = useProjects();
   return (
     <Select {...props}>
-      <SelectTrigger className="w-full">
+      <SelectTrigger className={cn("w-full", className)}>
         <SelectValue placeholder="Select project" />
       </SelectTrigger>
       <SelectContent>
--- src/components/work-entry.tsx
+++ src/components/work-entry.tsx
@@ -1,27 +1,60 @@
+import { useEffect, useState } from "react";
+
 import { type WorkEntry as WorkEntryType } from "@/db";
+import { cn } from "@/lib/utils";
 import { calcDuration } from "@/utils/calc-duration";
 import { formatTime } from "@/utils/format-time";
 
+import { ActiveWorkEntryActions } from "./active-work-entry-actions";
 import { ProjectSelect } from "./project-select";
 import { Input } from "./ui/input";
 import { WorkEntryActions } from "./work-entry-actions";
 
 export function WorkEntry({ entry }: { entry: WorkEntryType }) {
-  const duration = entry.endTime
-    ? calcDuration(entry.startTime, entry.endTime)
-    : 0;
+  const isActive = entry.endTime == null;
+  const [endTime, setEndTime] = useState(entry.endTime);
+  const [duration, setDuration] = useState(() =>
+    entry.endTime ? calcDuration(entry.startTime, entry.endTime) : 0,
+  );
+
+  useEffect(() => {
+    if (entry.endTime == null) {
+      const updateEndTime = () => {
+        const date = new Date();
+        setEndTime(date);
+        setDuration(calcDuration(entry.startTime, date));
+      };
+      updateEndTime();
+      const reg = setInterval(updateEndTime, 1000 * 60); // Every minute
+      return () => clearInterval(reg);
+    }
+  }, [entry]);
+
   return (
     <div className="grid grid-cols-[1fr_auto_1fr_1fr_1fr_auto] gap-2 items-center">
-      <Input disabled type="time" value={formatTime(entry.startTime)} />
-      to
       <Input
+        className={cn({ "border-green-600 bg-green-50": isActive })}
         disabled
         type="time"
-        value={entry.endTime ? formatTime(entry.endTime) : ""}
+        value={formatTime(entry.startTime)}
       />
-      <Input disabled value={duration} />
-      <ProjectSelect value={entry.project} />
-      <WorkEntryActions />
+      to
+      <Input
+        className={cn({ "border-green-600 bg-green-50": isActive })}
+        disabled
+        type="time"
+        value={endTime ? formatTime(endTime) : ""}
+      />
+      <Input
+        className={cn({ "border-green-600 bg-green-50": isActive })}
+        disabled
+        value={duration}
+      />
+      <ProjectSelect
+        className={cn({ "border-green-600 bg-green-50": isActive })}
+        value={entry.project}
+      />
+      {entry.endTime ? <WorkEntryActions /> : <ActiveWorkEntryActions />}
     </div>
   );
 }
